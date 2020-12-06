942d2ac4055dc864abf54da86b169d2bc7ae86b7
chance <chancesmith97@hotmail.com>
2020-12-05 17:59:44 -0700

    removed testing function

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import React from 'react';
 
 class App extends React.Component{
 
-  componentDidMount() {
-    fetch('http://localhost:3001/api/v1/days/1/events')
-    .then(res => res.json())
-    .then(res => console.log(res))
-  }
 
   render() {
     return (
