42110ab2af588ef48177d45f45b0403fc1a311e8
chance <chancesmith97@hotmail.com>
2020-12-20 12:29:28 -0700

    added dispatch to fetchDays to update store

--- src/actions/fetchDays.js
+++ src/actions/fetchDays.js
@@ -1,6 +1,9 @@
 export function fetchDays() {
-    console.log('inside fetch days')
-    fetch('http://localhost:3001/api/v1/days')
-    .then(resp => resp.json())
-    .then(resp => console.log(resp))
+    
+    return (dispatch) => {
+      console.log('inside fetch days')
+      fetch('http://localhost:3001/api/v1/days')
+      .then(resp => resp.json())
+      .then(resp => console.log(resp))
+    }
 }
