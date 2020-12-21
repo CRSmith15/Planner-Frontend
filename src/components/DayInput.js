74a302fdc8e189bae462021f47d78dd3e786d45d
chance <chancesmith97@hotmail.com>
2020-12-20 18:38:36 -0700

    added updating state functinoality in Day Input

--- src/components/DayInput.js
+++ src/components/DayInput.js
@@ -3,14 +3,23 @@ import React from 'react'
 
 class DayInput extends React.Component {
 
+    state = {date: '', time: ''}
+
+    handleChange = (event) => {
+        this.setState({
+           [event.target.name]: event.target.value 
+        })
+    }
+
     render(){
         return(
             <div>
                 <form>
                     <label>Date of Event: </label>
-                    <input type='text' placeholder='Date'/><br/>
+                    <input type='text' placeholder='Date' value={this.state.date} name='date' onChange={this.handleChange}/><br/>
                     <label>Time of Event: </label>
-                    <input type='text' placeholder='Time'/>
+                    <input type='text' placeholder='Time' value={this.state.time} name='time' onChange={this.handleChange}/><br/>
+                    <input type='submit'/>
                 </form>
             </div>
         )
