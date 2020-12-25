22f480a3c0d27a33f581064a3470924dde36a742
chance <chancesmith97@hotmail.com>
2020-12-25 11:52:01 -0700

    only rendered form on new page

--- src/containers/DaysContainer.js
+++ src/containers/DaysContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux';
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import DayList from '../components/DayList'
 import DayInput from '../components/DayInput'
 import ShowDay from '../components/ShowDay'
@@ -16,9 +16,11 @@ class DaysContainer extends React.Component {
     render() {
         return (
             <div>
-                <Route path='/days/new' component={DayInput}/>
-                <Route path='/days/:id' render={(routerProps) => <ShowDay {...routerProps} days={this.props.days}/>}/>
-                <Route exact path='/days' render={(routerProps) => <DayList {...routerProps} days={this.props.days}/>}/>
+                <Switch>
+                  <Route path='/days/new' component={DayInput}/>
+                  <Route path='/days/:id' render={(routerProps) => <ShowDay {...routerProps} days={this.props.days}/>}/>
+                  <Route exact path='/days' render={(routerProps) => <DayList {...routerProps} days={this.props.days}/>}/>
+                </Switch>
             </div>
         )
     }
