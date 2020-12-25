1914396522a7c20661da6a2d7834bd6ba8deee0c
chance <chancesmith97@hotmail.com>
2020-12-25 12:00:51 -0700

    created events container and rendered on show page for days

--- src/components/ShowDay.js
+++ src/components/ShowDay.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import {Redirect} from 'react-router-dom'
 
+import EventsContainer from '../containers/EventsContainer'
+
 
 const ShowDay = (props) => {
 
     let day = props.days[props.match.params.id - 1]
 
     return (
-        <li>
-            {day ? day.date : null} - {day ? day.time : null}
-        </li>
+        <div>
+          <h2>
+              {day ? day.date : null} - {day ? day.time : null}
+          </h2>
+          <EventsContainer/>
+        </div>
     )
 }
 
---
+++ src/containers/EventsContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class EventsContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                EventContainer
+            </div>
+        )
+    }
+}
+
+export default EventsContainer
